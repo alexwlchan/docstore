fae6e8c029d5d66839f19076994207a3fe45c1e5
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-07 19:20:31 +0100

    Get the safe_write method working

--- tests/test_concurrent_writer.py
+++ tests/test_concurrent_writer.py
@@ -1,25 +1,39 @@
 # -*- encoding: utf-8
 
-def safe_write(path, data):
-    with open(path, "wb") as outfile:
-        outfile.write(data)
+import os
+import pathlib
+import random
+import string
 
-    return path
+
+def safe_write(initial_path, data):
+    base, ext = os.path.splitext(initial_path)
+    random_part = ""
+
+    while True:
+        path = pathlib.Path(base + random_part + ext)
+        print(path)
+        try:
+            # Opening in exclusive write mode means we'll get an error if
+            # the file already exists.
+            with open(path, "xb") as outfile:
+                outfile.write(data)
+        except FileExistsError:
+            random_part = "_" + "".join(
+                random.choice(string.hexdigits) for _ in range(5))
+        else:
+            return path
 
 
 from concurrent.futures import as_completed, ThreadPoolExecutor
 
-from hypothesis import given
-from hypothesis.strategies import binary
 
-
-@given(binary())
-def test_writes_to_given_filename_by_default(tmpdir, data):
-    path = tmpdir / "greeting.txt"
-    assert safe_write(path, data = data) == path
+def test_writes_to_given_filename_by_default(tmpdir):
+    path = pathlib.Path(tmpdir / "greeting.txt")
+    assert safe_write(path, data = b"hello world") == path
 
     assert path.exists()
-    assert path.read_binary() == data
+    assert path.read_bytes() == b"hello world"
 
 
 def test_writes_to_different_filenames_if_running_concurrently(tmpdir):
@@ -34,7 +48,7 @@ def test_writes_to_different_filenames_if_running_concurrently(tmpdir):
     futures = []
     with ThreadPoolExecutor(max_workers=5) as executor:
         for i in range(thread_count):
-            future = executor.submit(safe_write, path, messages[i])
+            future = executor.submit(safe_write, pathlib.Path(path), messages[i])
             futures.append(future)
 
     filenames = set()
@@ -44,7 +58,7 @@ def test_writes_to_different_filenames_if_running_concurrently(tmpdir):
     assert len(filenames) == thread_count
 
     written_messages = {
-        f.read_binary() for f in filenames
+        f.read_bytes() for f in filenames
     }
 
     assert written_messages == set(messages)
