7acc0643e7da44c90b45b0addabb8fc9eb6de977
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-07 19:31:57 +0100

    Add an extra test for the safe file writer

--- tests/test_file_writer.py
+++ tests/test_file_writer.py
@@ -1,20 +1,23 @@
 # -*- encoding: utf-8
 
 from concurrent.futures import as_completed, ThreadPoolExecutor
+import pathlib
+
+import pytest
 
 from file_writer import safe_write
 
 
 def test_writes_to_given_filename_by_default(tmpdir):
     path = pathlib.Path(tmpdir / "greeting.txt")
-    assert safe_write(path, data = b"hello world") == path
+    assert safe_write(path, data=b"hello world") == path
 
     assert path.exists()
     assert path.read_bytes() == b"hello world"
 
 
 def test_writes_to_different_filenames_if_running_concurrently(tmpdir):
-    path = tmpdir / "greeting.txt"
+    path = pathlib.Path(tmpdir / "greeting.txt")
 
     thread_count = 10
 
@@ -25,7 +28,7 @@ def test_writes_to_different_filenames_if_running_concurrently(tmpdir):
     futures = []
     with ThreadPoolExecutor(max_workers=5) as executor:
         for i in range(thread_count):
-            future = executor.submit(safe_write, pathlib.Path(path), messages[i])
+            future = executor.submit(safe_write, path, messages[i])
             futures.append(future)
 
     filenames = set()
@@ -39,3 +42,10 @@ def test_writes_to_different_filenames_if_running_concurrently(tmpdir):
     }
 
     assert written_messages == set(messages)
+
+
+def test_fails_if_write_fails_for_reason_other_than_already_exists(tmpdir):
+    path = pathlib.Path(tmpdir / "a" / "b" / "c")
+
+    with pytest.raises(FileNotFoundError):
+        safe_write(path, data=b"hello world")
