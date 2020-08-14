6aee55f709844fa22c155dda88d3210e41ad9d93
Alex Chan <alex@alexwlchan.net>
2020-08-14 20:48:51 +0100

    I can write an encrypted file with Age

--- docstore.go
+++ docstore.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+    "bytes"
   "fmt"
+  "io"
   "io/ioutil"
+  "filippo.io/age"
+  "os"
   // "path/filepath"
   )
 
@@ -23,15 +27,42 @@ func (s FilesystemStore) Read(name string) (buffer []byte, err error) {
   return ioutil.ReadFile(name)
 }
 
-func Read(s Store) error {
-  return s.Write("hello.txt", []byte("hello world\n"))
+type EncryptedFilesystemStore struct {
+    root string
+}
+
+func (efs EncryptedFilesystemStore) Write(name string, buffer []byte, password string) error {
+    r, err := age.NewScryptRecipient(password)
+    if err != nil {
+        return err
+    }
+    r.SetWorkFactor(15)
+
+    f, err := os.Create(name)
+
+    if err != nil {
+        return err
+    }
+
+    buf := &bytes.Buffer{}
+    w, err := age.Encrypt(f, r)
+    if err != nil {
+        return err
+    }
+
+    if _, err = io.WriteString(w, "Hello world"); err != nil {
+        return err
+    }
+
+    fmt.Println(buf)
+
+    return nil
 }
 
 func main() {
-  s := FilesystemStore { root: "." }
-  err := Read(s)
-  fmt.Println(err)
-  data, _ := s.Read("hello.txt")
-  fmt.Println(string(data))
-	fmt.Println("Hello, 世界")
+  s := EncryptedFilesystemStore { root: "." }
+  s.Write("greeting.txt", []byte("hello world\n"), "password123")
+  // data, _ := s.Read("hello.txt")
+  // fmt.Println(string(data))
+    // fmt.Println("Hello, 世界")
 }
