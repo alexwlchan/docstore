4057a79c6f1d9a8d05478ea7f96f250c07c615c3
Alex Chan <alex@alexwlchan.net>
2020-09-23 20:42:38 +0100

    Add one more test to get to 100% coverage

--- v2/tests/test_models.py
+++ v2/tests/test_models.py
@@ -3,6 +3,7 @@ import json
 import uuid
 
 import attr
+import pytest
 
 from docstore.models import Document, File, Thumbnail, from_json, to_json
 
@@ -40,3 +41,9 @@ def test_can_serialise_document_to_json():
 
     documents = [Document(files=[f])]
     assert from_json(to_json(documents)) == documents
+
+
+@pytest.mark.parametrize("documents", [[1, 2, 3], {"a", "b", "c"}])
+def test_to_json_with_bad_list_is_typeerror(documents):
+    with pytest.raises(TypeError, match=r"Expected type List\[Document\]!"):
+        to_json(documents)
