fe3499565ee327df0c402df97cbefbddfc82fc39
Alex Chan <alex@alexwlchan.net>
2020-09-29 08:11:52 +0100

    Allow for mismatched order in this test case

--- tests/test_kmeans.py
+++ tests/test_kmeans.py
@@ -7,4 +7,5 @@ def test_kmeans_on_two_points():
 
 def test_kmeans_on_four_points():
     points = [(0, 0), (1, 1), (9, 9), (10, 10)]
-    assert kmeans(points, k=2) == [(0.5, 0.5), (9.5, 9.5)]
+    result = kmeans(points, k=2)
+    assert result == [(0.5, 0.5), (9.5, 9.5)] or result == [(9.5, 9.5), (0.5, 0.5)]
