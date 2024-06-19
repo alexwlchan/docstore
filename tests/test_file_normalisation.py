24707f7c1cbb651b852b092cb1cd77da11aa5acb
Alex Chan <alex@alexwlchan.net>
2024-06-19 06:41:04 +0100

    Fix types in `test_file_normalisation.py`
    22 errors in 8 files ~> 18 errors in 7 files

--- tests/test_file_normalisation.py
+++ tests/test_file_normalisation.py
@@ -1,47 +1,48 @@
 import concurrent.futures
 import os
+import pathlib
 
 from docstore.file_normalisation import normalised_filename_copy
 
 
-def test_copies_a_file(tmpdir):
+def test_copies_a_file(tmpdir: pathlib.Path) -> None:
     src = tmpdir / "src.txt"
     dst = tmpdir / "dst.txt"
 
-    src.write("Hello world")
+    src.write_text("Hello world")
     assert not dst.exists()
 
-    normalised_filename_copy(src=src, dst=dst)
+    normalised_filename_copy(src=str(src), dst=str(dst))
 
-    assert dst.read() == "Hello world"
+    assert dst.read_text() == "Hello world"
 
 
-def test_creates_intermediate_directories(tmpdir):
+def test_creates_intermediate_directories(tmpdir: pathlib.Path) -> None:
     src = tmpdir / "src.txt"
     dst = tmpdir / "1" / "2" / "3" / "dst.txt"
 
-    src.write("Hello world")
+    src.write_text("Hello world")
     assert not dst.exists()
 
-    normalised_filename_copy(src=src, dst=dst)
+    normalised_filename_copy(src=str(src), dst=str(dst))
 
-    assert dst.read() == "Hello world"
+    assert dst.read_text() == "Hello world"
 
 
-def test_copies_multiple_files_to_the_same_dst(tmpdir):
+def test_copies_multiple_files_to_the_same_dst(tmpdir: pathlib.Path) -> None:
     src1 = tmpdir / "src1.txt"
     src2 = tmpdir / "src2.txt"
     src3 = tmpdir / "src3.txt"
 
     dst = tmpdir / "dst.txt"
 
-    src1.write("Hello world")
-    src2.write("Bonjour le monde")
-    src3.write("Hallo Welt")
+    src1.write_text("Hello world")
+    src2.write_text("Bonjour le monde")
+    src3.write_text("Hallo Welt")
 
-    normalised_filename_copy(src=src1, dst=dst)
-    normalised_filename_copy(src=src2, dst=dst)
-    normalised_filename_copy(src=src3, dst=dst)
+    normalised_filename_copy(src=str(src1), dst=str(dst))
+    normalised_filename_copy(src=str(src2), dst=str(dst))
+    normalised_filename_copy(src=str(src3), dst=str(dst))
 
     assert len([f for f in os.listdir(tmpdir) if "dst" in f]) == 3
 
@@ -52,20 +53,20 @@ def test_copies_multiple_files_to_the_same_dst(tmpdir):
     assert dst_contents == {"Hello world", "Bonjour le monde", "Hallo Welt"}
 
 
-def test_copies_multiple_files_concurrently(tmpdir):
+def test_copies_multiple_files_concurrently(tmpdir: pathlib.Path) -> None:
     src1 = tmpdir / "src1.txt"
     src2 = tmpdir / "src2.txt"
     src3 = tmpdir / "src3.txt"
 
     dst = tmpdir / "dst.txt"
 
-    src1.write("Hello world")
-    src2.write("Bonjour le monde")
-    src3.write("Hallo Welt")
+    src1.write_text("Hello world")
+    src2.write_text("Bonjour le monde")
+    src3.write_text("Hallo Welt")
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
         futures = {
-            executor.submit(normalised_filename_copy, src=s, dst=dst)
+            executor.submit(normalised_filename_copy, src=str(s), dst=str(dst))
             for s in (src1, src2, src3)
         }
         concurrent.futures.wait(futures)
